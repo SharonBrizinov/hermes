d2781954ed9d76b8fab405f00108715da9cb4eaf
Sharon Brizinov <SharonBrizinov@users.noreply.github.com>
2023-08-27 11:41:51 +0300

    Update JSObject.cpp

--- lib/VM/JSObject.cpp
+++ lib/VM/JSObject.cpp
@@ -1394,9 +1394,9 @@ CallResult<bool> JSObject::putNamedWithReceiver_RJS(
     }
 
     if (LLVM_UNLIKELY(desc.flags.proxyObject)) {
-      assert(
-          !opFlags.getMustExist() &&
-          "MustExist cannot be used with Proxy objects");
+      //assert(
+      //    !opFlags.getMustExist() &&
+      //    "MustExist cannot be used with Proxy objects");
       CallResult<bool> setRes = JSProxy::setNamed(
           runtime.makeHandle(propObj), runtime, name, valueHandle, receiver);
       if (LLVM_UNLIKELY(setRes == ExecutionStatus::EXCEPTION)) {
@@ -1653,9 +1653,9 @@ CallResult<bool> JSObject::putComputedWithReceiver_RJS(
     }
 
     if (LLVM_UNLIKELY(desc.flags.proxyObject)) {
-      assert(
-          !opFlags.getMustExist() &&
-          "MustExist cannot be used with Proxy objects");
+      //assert(
+      //    !opFlags.getMustExist() &&
+      //    "MustExist cannot be used with Proxy objects");
       CallResult<Handle<>> key = toPropertyKey(runtime, nameValPrimitiveHandle);
       if (key == ExecutionStatus::EXCEPTION)
         return ExecutionStatus::EXCEPTION;
